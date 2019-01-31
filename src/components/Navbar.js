b147b7463cbab4c58b4c9a83a44cd10675738366
martin_marinov <martin_n_marinov@mail.bg>
2019-01-31 15:56:54 +0200

    hamburger menu working on mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,80 +10,117 @@ export class Navbar extends Component {
     openModal: PropTypes.func.isRequired
   }
 
+  state = {
+    burgerButtonClicked: false,
+    burgerMenuDisabled: false
+  }
+
   constructor(props) {
     super(props);
     this.scrollToTop = this.scrollToTop.bind(this);
+    this.burgerButtonClick = this.burgerButtonClick.bind(this);
+  }
+
+  componentDidMount() {
+    const mediaQuery = window.matchMedia('(min-width: 450px)');
+    const mediaQueryCallback = (changed) => {
+      if (changed.matches) this.setState({ burgerMenuDisabled: true, burgerButtonClicked: true });
+      else this.setState({ burgerMenuDisabled: false, burgerButtonClicked: false });
+    }
+
+    mediaQueryCallback(mediaQuery);
+    mediaQuery.addListener(mediaQueryCallback);
   }
 
   scrollToTop() {
     scroll.scrollToTop();
-  };
+  }
+
+  burgerButtonClick() {
+    if (this.state.burgerMenuDisabled) return;
+    this.setState((state) => {
+      return { burgerButtonClicked: !state.burgerButtonClicked };
+    });
+  }
 
   render() {
+    const { burgerButtonClicked } = this.state;
+
     return (
       <nav className="section-header">
         <div className="section-header-inner">
-          <img
-            src={LogoHorizontal}
-            alt="Logo Horizontal"
-            onClick={this.scrollToTop}
-          />
+          <div className="section-header-burger-container">
+            <div className="section-header-logo-container">
+              <img
+                src={LogoHorizontal}
+                alt="Logo Horizontal"
+                onClick={this.scrollToTop}
+              />
+            </div>
 
-          <div className="hamburger-menu">
-            <div></div>
-            <div></div>
-            <div></div>
+            <div className="hamburger-menu" onClick={this.burgerButtonClick}>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
           </div>
 
-          <ul className="section-header-nav">
-            <li className="nav-item">
-              <Link
-                activeClass="active"
-                to="talents-wanted-section"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-              >
-                Talents Wanted
-              </Link>
-            </li>
+          {
+            burgerButtonClicked &&
+            <ul className="section-header-nav">
+              <li className="nav-item">
+                <Link
+                  activeClass="active"
+                  to="talents-wanted-section"
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                  duration={500}
+                  onClick={this.burgerButtonClick}
+                >
+                  Talents Wanted
+                  </Link>
+              </li>
 
-            <li className="nav-item">
-              <Link
-                activeClass="active"
-                to="how-we-work-section"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-              >
-                How We Work
-              </Link>
-            </li>
+              <li className="nav-item">
+                <Link
+                  activeClass="active"
+                  to="how-we-work-section"
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                  duration={500}
+                  onClick={this.burgerButtonClick}
+                >
+                  How We Work
+                  </Link>
+              </li>
 
-            <li className="nav-item">
-              <Link
-                activeClass="active"
-                to="team-section"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-              >
-                Team
-              </Link>
-            </li>
+              <li className="nav-item">
+                <Link
+                  activeClass="active"
+                  to="team-section"
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                  duration={500}
+                  onClick={this.burgerButtonClick}
+                >
+                  Team
+                  </Link>
+              </li>
 
-            <li className="nav-item button" >
-              <a
-                className="nav-join-us-a"
-                href={`mailto:${companyInfo.email}?Subject=${companyInfo['email-subjet']}`}
-              >
-                Join our team
-              </a>
-            </li>
-          </ul>
+              <li className="nav-item button" >
+                <a
+                  className="nav-join-us-a"
+                  href={`mailto:${companyInfo.email}?Subject=${companyInfo['email-subjet']}`}
+                >
+                  Join our team
+                </a>
+              </li>
+            </ul>
+          }
+
         </div>
       </nav>
     );
